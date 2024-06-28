79f51a39127398126ed604344a55f4c4051c0d59
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-28 09:17:53 +0000

    Updating to latest FlutterFlow output.

--- lib/component/transaction_detail_view/transaction_detail_view_model.dart
+++ lib/component/transaction_detail_view/transaction_detail_view_model.dart
@@ -1,4 +1,5 @@
 import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -6,6 +7,7 @@ import 'transaction_detail_view_widget.dart' show TransactionDetailViewWidget;
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class TransactionDetailViewModel
