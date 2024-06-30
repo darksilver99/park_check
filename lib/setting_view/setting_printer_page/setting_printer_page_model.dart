7b2d39c76c63e1b9789377d8cc18261c82047a3a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-30 05:46:10 +0000

    Updating to latest FlutterFlow output.

--- lib/setting_view/setting_printer_page/setting_printer_page_model.dart
+++ lib/setting_view/setting_printer_page/setting_printer_page_model.dart
@@ -3,7 +3,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/custom_code/actions/index.dart' as actions;
 import 'setting_printer_page_widget.dart' show SettingPrinterPageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -12,22 +11,9 @@ import 'package:provider/provider.dart';
 
 class SettingPrinterPageModel
     extends FlutterFlowModel<SettingPrinterPageWidget> {
-  ///  Local state fields for this page.
-
-  List<dynamic> printerList = [];
-  void addToPrinterList(dynamic item) => printerList.add(item);
-  void removeFromPrinterList(dynamic item) => printerList.remove(item);
-  void removeAtIndexFromPrinterList(int index) => printerList.removeAt(index);
-  void insertAtIndexInPrinterList(int index, dynamic item) =>
-      printerList.insert(index, item);
-  void updatePrinterListAtIndex(int index, Function(dynamic) updateFn) =>
-      printerList[index] = updateFn(printerList[index]);
-
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // Stores action output result for [Custom Action - getPrinterList] action in SettingPrinterPage widget.
-  List<dynamic>? printerResultList;
   // Model for MainBackgroundView component.
   late MainBackgroundViewModel mainBackgroundViewModel;
 
--- lib/setting_view/setting_printer_page/setting_printer_page_widget.dart
+++ lib/setting_view/setting_printer_page/setting_printer_page_widget.dart
@@ -3,7 +3,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -31,8 +30,6 @@ class _SettingPrinterPageWidgetState extends State<SettingPrinterPageWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      _model.printerResultList = await actions.getPrinterList();
-      _model.printerList = _model.printerResultList!.toList().cast<dynamic>();
       setState(() {});
     });
   }
@@ -46,6 +43,8 @@ class _SettingPrinterPageWidgetState extends State<SettingPrinterPageWidget> {
 
   @override
   Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
     return GestureDetector(
       onTap: () => _model.unfocusNode.canRequestFocus
           ? FocusScope.of(context).requestFocus(_model.unfocusNode)
@@ -96,97 +95,12 @@ class _SettingPrinterPageWidgetState extends State<SettingPrinterPageWidget> {
                 mainAxisSize: MainAxisSize.max,
                 children: [
                   Expanded(
-                    child: Builder(
-                      builder: (context) {
-                        final printerListView = _model.printerList.toList();
-                        return ListView.separated(
-                          padding: EdgeInsets.fromLTRB(
-                            0,
-                            16.0,
-                            0,
-                            16.0,
-                          ),
-                          scrollDirection: Axis.vertical,
-                          itemCount: printerListView.length,
-                          separatorBuilder: (_, __) => SizedBox(height: 8.0),
-                          itemBuilder: (context, printerListViewIndex) {
-                            final printerListViewItem =
-                                printerListView[printerListViewIndex];
-                            return Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(
-                                  16.0, 0.0, 16.0, 0.0),
-                              child: Container(
-                                width: double.infinity,
-                                height: 100.0,
-                                decoration: BoxDecoration(
-                                  color: FlutterFlowTheme.of(context)
-                                      .secondaryBackground,
-                                  borderRadius: BorderRadius.circular(24.0),
-                                ),
-                                child: Padding(
-                                  padding: EdgeInsets.all(24.0),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      Expanded(
-                                        child: Padding(
-                                          padding:
-                                              EdgeInsetsDirectional.fromSTEB(
-                                                  0.0, 0.0, 8.0, 0.0),
-                                          child: Text(
-                                            'ชื่อเครื่อง',
-                                            maxLines: 2,
-                                            style: FlutterFlowTheme.of(context)
-                                                .bodyMedium
-                                                .override(
-                                                  fontFamily: 'Readex Pro',
-                                                  fontSize: 16.0,
-                                                  letterSpacing: 0.0,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                          ),
-                                        ),
-                                      ),
-                                      FFButtonWidget(
-                                        onPressed: () {
-                                          print('Button pressed ...');
-                                        },
-                                        text: 'เชื่อมต่อ',
-                                        options: FFButtonOptions(
-                                          height: 40.0,
-                                          padding:
-                                              EdgeInsetsDirectional.fromSTEB(
-                                                  24.0, 0.0, 24.0, 0.0),
-                                          iconPadding:
-                                              EdgeInsetsDirectional.fromSTEB(
-                                                  0.0, 0.0, 0.0, 0.0),
-                                          color: FlutterFlowTheme.of(context)
-                                              .primary,
-                                          textStyle:
-                                              FlutterFlowTheme.of(context)
-                                                  .titleSmall
-                                                  .override(
-                                                    fontFamily: 'Readex Pro',
-                                                    color: Colors.white,
-                                                    letterSpacing: 0.0,
-                                                  ),
-                                          elevation: 3.0,
-                                          borderSide: BorderSide(
-                                            color: Colors.transparent,
-                                            width: 1.0,
-                                          ),
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
+                    child: Container(
+                      width: double.infinity,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).secondaryBackground,
+                      ),
                     ),
                   ),
                 ],
