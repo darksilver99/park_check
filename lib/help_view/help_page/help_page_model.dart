8c73801083d34ea84fb12b914d8f15cb09f60da1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-26 10:54:27 +0000

    Updating to latest FlutterFlow output.

--- lib/help_view/help_page/help_page_model.dart
+++ lib/help_view/help_page/help_page_model.dart
@@ -7,16 +7,22 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import '/help_view/help_detail_view/help_detail_view_widget.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'help_page_widget.dart' show HelpPageWidget;
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
 
 class HelpPageModel extends FlutterFlowModel<HelpPageWidget> {
+  ///  Local state fields for this page.
+
+  int total = 0;
+
   ///  State fields for stateful widgets in this page.
 
   // Model for MainBackgroundView component.
   late MainBackgroundViewModel mainBackgroundViewModel;
+  List<HelpListRecord>? listViewPreviousSnapshot;
 
   @override
   void initState(BuildContext context) {
