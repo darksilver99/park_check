18af93a3c879d8334166df8adfdce918b17e5741
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-26 11:02:59 +0000

    Updating to latest FlutterFlow output.

--- lib/help_view/help_page/help_page_model.dart
+++ lib/help_view/help_page/help_page_model.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/component/main_background_view/main_background_view_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
@@ -7,8 +8,10 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import '/help_view/help_detail_view/help_detail_view_widget.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'help_page_widget.dart' show HelpPageWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:webviewx_plus/webviewx_plus.dart';
@@ -20,6 +23,8 @@ class HelpPageModel extends FlutterFlowModel<HelpPageWidget> {
 
   ///  State fields for stateful widgets in this page.
 
+  // Stores action output result for [Firestore Query - Query a collection] action in HelpPage widget.
+  int? totalHelpResult;
   // Model for MainBackgroundView component.
   late MainBackgroundViewModel mainBackgroundViewModel;
   List<HelpListRecord>? listViewPreviousSnapshot;
